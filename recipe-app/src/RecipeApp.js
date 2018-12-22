2ead7313a22c4386f7491ec9a7f6c44de5c97875
DanielW27 <32105320+DanielW27@users.noreply.github.com>
2018-12-21 20:35:43 -0500

    Added basic components for the recipes

---
+++ recipe-app/src/RecipeApp.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import Recipe from './Recipe';
+import './RecipeApp.css';
+
+class RecipeApp extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Recipe title="pasta" 
+        ingredients={['flour', 'water']}
+        instructions="mix ingredients"
+        img="spaghetti.jpg"/>
+      </div>
+    );
+  }
+}
+
+export default RecipeApp;
